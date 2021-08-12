ce384d7cdeae2b48f10fd4e05cd7337d8979f958
Grit <perepadin00@gmail.com>
2021-08-12 14:18:44 +0300

    исправил case блоки

--- src/utils.js
+++ src/utils.js
@@ -11,12 +11,14 @@ const RenderPosition = {
 
 const render = (container, element, place) => {
   switch (place) {
-    case RenderPosition.AFTERBEGIN:
+    case RenderPosition.AFTERBEGIN: {
       container.prepend(element);
       break;
-    case RenderPosition.BEFOREEND:
+    }
+    case RenderPosition.BEFOREEND: {
       container.append(element);
       break;
+    }
   }
 };
 
