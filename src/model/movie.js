e26c2c04426b9b62aec4a9a3d93846c335c66d1d
Grit <perepadin00@gmail.com>
2021-09-17 15:39:02 +0300

    удалил моки

--- src/model/movie.js
+++ src/model/movie.js
@@ -1,6 +1,7 @@
 import AbstractObserver from '../utils/abstract-observer.js';
-import { createComment, deleteComment, getCommentsByIds } from '../mock/newfilm.js';
-
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration.js';
+dayjs.extend(duration);
 
 class Films extends AbstractObserver {
   constructor() {
@@ -22,51 +23,6 @@ class Films extends AbstractObserver {
     this._comments = comments.slice();
     this._notify(updateType);
   }
-
-  getFilmComments(id) {
-    const { comments } = this._films.find((film) => film.id === id);
-    return getCommentsByIds(comments);
-  }
-
-  deleteComment(updateType, { film, commentId }) {
-    deleteComment(commentId);
-
-    const updatedFilm = {
-      ...film,
-      comments: film.comments.filter((id) => id !== commentId),
-    };
-
-    this.updateFilm(updateType, updatedFilm);
-  }
-
-  createComment(updateType, { film, newComment }) {
-    const { id: commentId } = createComment(newComment);
-
-    const updatedFilm = {
-      ...film,
-      comments: [...film.comments, commentId],
-    };
-
-    this.updateFilm(updateType, updatedFilm);
-  }
-
-  updateFilm(updateType, update) {
-    const index = this._films.findIndex((film) => film.id === update.id);
-
-    if (index === -1) {
-      throw new Error('Can\'t update unexisting film');
-    }
-
-    this._films = [
-      ...this._films.slice(0, index),
-      update,
-      ...this._films.slice(index + 1),
-    ];
-
-    this._notify(updateType, update);
-  }
-
-
 
   static adaptCommentToClient(comment) {
     const clientComment = { };
